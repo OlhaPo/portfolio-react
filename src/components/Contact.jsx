183021ca4961948fba81c8460f783da4dc232486
Olya <ponomaryova.olya.y@gmail.com>
2023-04-28 17:44:18 +0200

    Connect contact form with getform platform

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ const Contact = () => {
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
-      <form action="" method="POST" encType="multipart/form-data">
+      <form
+        action="https://getform.io/f/27781151-f3b7-4001-bfa4-e92362fedaa4"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <div className="grid md:grid-cols-2 gap-4 w-full py-2 ">
           <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Name</label>
